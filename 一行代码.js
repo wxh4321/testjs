949236a52477ab8848978d91cfbbf36eb92359ba
wxh4321 <hongtianyi@outlook.com>
2021-08-13 16:23:56 +0800

    add new code

--- 一行代码.js
+++ 一行代码.js
@@ -1,1 +1,2 @@
+// 进制转换
 function baseMul(num,base){const result=[];do{result.push(num%base);num=Math.floor(num/=base);}while(num>0);let resultStr='';while(result.length>0){resultStr+=result.pop();}return resultStr;}
