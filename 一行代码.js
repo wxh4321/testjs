4ad317d866d81ec24b85d42561de12e6fb77d2ed
wxh <hongtianyi@outlook.com>
2021-08-16 23:38:11 +0800

    fix

--- 一行代码.js
+++ 一行代码.js
@@ -1,2 +1,4 @@
+// 回文序列
+function isPalindrome(word){const s = [];for(let i=0;i<word.length;i++){s.push(word[i]);};let result='';while(s.length>0){result+=s.pop();}if(word===result){return true;}else{return false;}}
 // 进制转换
 function baseMul(num,base){const result=[];do{result.push(num%base);num=Math.floor(num/=base);}while(num>0);let resultStr='';while(result.length>0){resultStr+=result.pop();}return resultStr;}
