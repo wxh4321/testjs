f8a3b2fb35afcc8c6b583c82dadbceb0e25f7440
wxh <hongtianyi@outlook.com>
2021-08-18 01:00:42 +0800

    jsonp

--- 一行代码.js
+++ 一行代码.js
@@ -2,3 +2,5 @@
 function isPalindrome(word){const s = [];for(let i=0;i<word.length;i++){s.push(word[i]);};let result='';while(s.length>0){result+=s.pop();}if(word===result){return true;}else{return false;}}
 // 进制转换
 function baseMul(num,base){const result=[];do{result.push(num%base);num=Math.floor(num/=base);}while(num>0);let resultStr='';while(result.length>0){resultStr+=result.pop();}return resultStr;}
+// 阶乘
+function factorial(n){if(n===0){return 1;}else{return n*factorial(n-1)}}
